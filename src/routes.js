52e33b2cd77fdd1d29cdde808730158eb4004050
Gabriel Perussolo Della Paolera <gabrielmv2309@gmail.com>
2025-05-17 16:06:35 -0300

    Starting new project repos

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Main from './Pages/Main';
+import Repositorio from './Pages/Repositorio';
+
+
+export default function AppRoutes() {
+return (
+
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/repositorio/:repositorio" element={<Repositorio />} />
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
